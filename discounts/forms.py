12938fb5038999d378e31745589bd5e420b52871
Oduntan Balogun <oduntan@live.com>
2019-05-07 12:29:27 +0100

    added validation methods for Discount Form

--- discounts/forms.py
+++ discounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext as _
 from .models import Discounts
 
 
@@ -8,3 +9,20 @@ class DiscountForm(forms.ModelForm):
         fields = [
             'code', 'percentage', 'amount', 'expiry_date'
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        percentage = cleaned_data.get('percentage')
+        amount = cleaned_data.get('amount')
+
+        if percentage and amount:
+            raise forms.ValidationError(_('Percentage and amount cannot be supplied at the same time'))
+
+        if not percentage and not amount:
+            raise forms.ValidationError(_('Please provide an amount or a percentage'))
+
+    def clean_percentage(self):
+        percentage = self.cleaned_data['percentage']
+        if percentage and percentage > 50:
+            raise forms.ValidationError(_('Discount percentage cannot be more than 50%'))
+        return percentage
