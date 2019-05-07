e4725c1c80dc13ec69cafdfa5f0f70096527e776
Oduntan Balogun <oduntan@live.com>
2019-05-07 13:09:41 +0100

    Minor changes. Added blank=True to percentage and amount to aid validation. Added __str__ function

--- discounts/models.py
+++ discounts/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 class Discounts(SafeDeleteModel):
     code = models.CharField('code', max_length=50, unique=True)
-    percentage = models.IntegerField('percentage', null=True)
-    amount = models.DecimalField('amount', null=True, max_digits=10, decimal_places=2)
+    percentage = models.IntegerField('percentage', null=True, blank=True)
+    amount = models.DecimalField('amount', null=True, blank=True, max_digits=10, decimal_places=2)
     created_at = models.DateTimeField('created at', auto_now_add=True)
     expiry_date = models.DateField('expiry date')
     is_active = models.BooleanField('is active', default=True)
@@ -17,3 +17,6 @@ class Discounts(SafeDeleteModel):
         if self.percentage:
             return "{}%".format(self.percentage)
         return "N{}".format(self.amount)
+
+    def __str__(self):
+        return self.code
