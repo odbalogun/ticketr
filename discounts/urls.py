35f1273c1f31ea6ae0c6025caa099cd0d8558beb
Oduntan Balogun <oduntan@live.com>
2019-05-07 12:34:41 +0100

    Added views and urls for discount app

---
+++ discounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import DiscountCreateView, DiscountListView, DiscountDeleteView, DiscountDetailView, DiscountUpdateView
+
+app_name = 'deals'
+
+urlpatterns = [
+    path('', DiscountListView.as_view(), name='list'),
+    path('create/', DiscountCreateView.as_view(), name='create'),
+    path('<pk>/', DiscountDetailView.as_view(), name='detail'),
+    path('<pk>/delete/', DiscountDeleteView.as_view(), name='delete'),
+    path('<pk>/update/', DiscountUpdateView.as_view(), name='update'),
+]
--- discounts/views.py
+++ discounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.views.generic import UpdateView, CreateView, ListView, DetailView, DeleteView
+from .models import Discounts
+from .forms import DiscountForm
 
-# Create your views here.
+
+class DiscountCreateView(CreateView):
+    form_class = DiscountForm
+    template_name = "discounts/create.html"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.created_by = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(reverse_lazy("discounts:list"))
+
+
+class DiscountListView(ListView):
+    template_name = "discounts/list.html"
+    queryset = Discounts.objects.all()
+
+
+class DiscountUpdateView(UpdateView):
+    form_class = DiscountForm
+    model = Discounts
+    success_url = reverse_lazy("discounts:list")
+    template_name = "discounts/update.html"
+
+
+class DiscountDetailView(DetailView):
+    queryset = Discounts.objects.all()
+    template_name = "discounts/detail.html"
+
+
+class DiscountDeleteView(DeleteView):
+    model = Discounts
+    template_name = "discounts/confirm_delete.html"
+    success_url = reverse_lazy("discounts:list")
