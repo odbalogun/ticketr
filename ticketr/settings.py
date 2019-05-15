5eacd0b19355f597df64b3b63a57b484685c0bb9
Oduntan Balogun <oduntan@live.com>
2019-05-15 13:30:44 +0100

    Added admin interface

--- ticketr/settings.py
+++ ticketr/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    'grappelli',
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
