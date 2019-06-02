355b118d51a9a82c186ec147e88bd27efeff5c54
Oduntan Balogun <oduntan@live.com>
2019-06-03 00:39:21 +0100

    Removed PageDealView

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, SignupPageView, VerifyEmailView, TestPageView, LoginPageView, LogoutPageView, ProfilePageView, CheckoutPageView, DealsPageView
+from .views import HomePageView, SignupPageView, VerifyEmailView, TestPageView, LoginPageView, LogoutPageView, ProfilePageView, CheckoutPageView
 
 app_name = 'pages'
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('talent/', TestPageView.as_view(), name='talent'),
     path('profile/', ProfilePageView.as_view(), name='profile'),
     path('checkout/', CheckoutPageView.as_view(), name='checkout'),
-    path('deals/', DealsPageView.as_view(), name='deals'),
     path('signup/', SignupPageView.as_view(), name='signup'),
     path('login/', LoginPageView.as_view(), name='login'),
     path('logout/', LogoutPageView.as_view(), name='logout'),
