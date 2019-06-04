5b2dfb4779167b95c94b32dfb8864a6a4f0daaf7
Oduntan Balogun <oduntan@live.com>
2019-06-04 17:00:25 +0100

    Worked on Movies application. Added admin and user views

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Cinema, Movie
 
-# Register your models here.
+
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'state')
+    list_filter = ('city', 'state')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'display_price_2d', 'display_price_3d', 'rating')
+    list_filter = ('rating', )
+    exclude = ('slug', 'created_by', 'created_at')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'created_by', None) is None:
+            obj.created_by = request.user
+        obj.save()
+
+    # for prices
+    def display_price_2d(self, object):
+        return object.display_price_2d
+
+    def display_price_3d(self, object):
+        return object.display_price_3d
+
+    display_price_2d.short_description = '2D Price'
+    display_price_2d.admin_order_field = 'price_2d'
+
+    display_price_3d.short_description = '3D Price'
+    display_price_3d.admin_order_field = 'price_3d'
+
+
+
+admin.site.register(Cinema, CinemaAdmin)
+admin.site.register(Movie, MovieAdmin)
---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import MovieDetailView, MovieListView
+
+app_name = 'deals'
+
+urlpatterns = [
+    path('', MovieListView.as_view(), name='list'),
+    path('<pk>/', MovieDetailView.as_view(), name='detail'),
+]
