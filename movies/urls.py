6e9b41538164f96bb1e87a5fe1426aa6d3a4ff96
Oduntan Balogun <oduntan@live.com>
2019-06-04 22:03:05 +0100

    Added app_name to avoid conflict with other modules

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import MovieDetailView, MovieListView
 
-app_name = 'deals'
+app_name = 'movies'
 
 urlpatterns = [
     path('', MovieListView.as_view(), name='list'),
