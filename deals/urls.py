73ef85a54b04fb3d6f0ada83fc8754cd1edc65ee
Oduntan Balogun <oduntan@live.com>
2019-06-18 01:08:39 +0100

    Added url for purchase tickets

--- deals/urls.py
+++ deals/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import DealCreateView, DealListView, manage_deal_categories, DealDeleteView, DealDetailView, DealUpdateView
+from .views import DealCreateView, DealListView, manage_deal_categories, DealDeleteView, DealDetailView, \
+    DealUpdateView, purchase_tickets
 
 app_name = 'deals'
 
 urlpatterns = [
     path('', DealListView.as_view(), name='list'),
+    path('purchase-tickets/', purchase_tickets, name='purchase-tickets'),
     path('create/', DealCreateView.as_view(), name='create'),
     path('<pk>/', DealDetailView.as_view(), name='detail'),
     path('<pk>/delete/', DealDeleteView.as_view(), name='delete'),
